bdded3796ea9567d9b6bf9f5fa635a5f89204aff
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-20 08:39:12 +0300

    update tests

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,28 +1,25 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
-  setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+  setup { @quote = quotes(:first) }
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
   end
 
   test "Creating a new quote" do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes"
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New qupte"
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create quote"
     fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
@@ -32,9 +29,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
