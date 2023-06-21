0d359963f4fb802538db47f6d0c56c9eb5fd89b3
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-21 09:30:24 +0300

    add quotes order

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: %i[show edit update destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered # works with scope property in quote model
   end
 
   def new
