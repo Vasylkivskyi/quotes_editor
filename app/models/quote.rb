b55458097a101dc9e075e2c5e6c8ebfe1494aa6f
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-22 22:27:59 +0300

    add action cable destroy quotes broadcasting

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -20,6 +20,9 @@ class Quote < ApplicationRecord
   # The partial default value is equal to calling to_partial_path on an instance of the model, which by default in Rails for our Quote model is equal to "quotes/quote".
   # The locals default value is equal to { model_name.element.to_sym => self } which, in in the context of our Quote model, is equal to { quote: self }.
 
-  # broadcast ufter quote update (short version)
+  # broadcast ufter quote was updated (short version)
   after_update_commit -> { broadcast_replace_to "quotes" }
+
+  # broadcast ufter quote was deleted (short version)
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
