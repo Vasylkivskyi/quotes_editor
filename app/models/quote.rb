cec1db11f3c549bcf7bb29ddd93bd48fb4336145
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-26 08:18:19 +0300

    add line_item_dates model and seeds

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,7 @@
 class Quote < ApplicationRecord
   belongs_to :company
   validates :name, presence: true
-
+  has_many :line_item_dates, dependent: :destroy
   scope :ordered, -> { order(id: :desc) }
 
   # Long detailed version
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: %i[index show]
+  end
 end
