ba11d7ea747c508f90850b200cd43e07fc0dafe1
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-30 22:47:57 +0300

    add turbo date update

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -2,26 +2,6 @@ class LineItemDatesController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date, only: %i[edit update destroy]
 
-  # def new
-  #   @line_item_date = @quote.line_item_dates.build
-  # end
-
-  # def create
-  #   @line_item_date = @quote.line_item_dates.build(line_item_date_params)
-  #   respond_to do |format|
-  #     if @line_item_date.save
-  #       format.html do
-  #         redirect_to quote_path(@quote),
-  #                     notice: "Date was successfully created."
-  #       end
-  #       format.turbo_stream do
-  #         flash.now[:notice] = "Date was successfully created."
-  #       end
-  #     else
-  #       render :new, status: :unprocessable_entity
-  #     end
-  #   end
-  # end
   def new
     @line_item_date = @quote.line_item_dates.build
   end
@@ -49,7 +29,16 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        format.html do
+          redirect_to quote_path(@quote),
+                      notice: "Date was successfully updated."
+        end
+
+        format.turbo_stream do
+          flash.now[:notice] = "Date was successfully updated."
+        end
+      end
     else
       render :edit, status: :unrocessable_entity
     end
