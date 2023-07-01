15ed66769e015729b82c0aa7f0608dc606b57f8c
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-07-01 14:19:27 +0300

    fix tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get root_path
+    get new_user_session_path
     assert_response :success
   end
 end
