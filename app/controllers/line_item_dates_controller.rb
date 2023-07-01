035b123fb9152d027529305ee7ccfd3a1ef6846a
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-07-01 14:05:18 +0300

    add turbo destroy

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -46,8 +46,15 @@ class LineItemDatesController < ApplicationController
 
   def destroy
     @line_item_date.destroy
-
-    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+    respond_to do |format|
+      format.html do
+        redirect_to quote_path(@quote),
+                    notice: "Date was successfully }destroyed."
+      end
+      format.turbo_stream do
+        flash.now[:notice] = "Date was successfully destroyed."
+      end
+    end
   end
 
   private
