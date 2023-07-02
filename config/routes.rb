92dc9c2e672056d4abe3686e4acfb219475a203b
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-07-02 08:21:29 +0300

    add line_item routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :quotes do
-    resources :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
